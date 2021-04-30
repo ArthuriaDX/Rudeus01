5ab281ba27d78bdc766681d06fba3b796d92d9b9
Aliwoto <aminnimaj@gmail.com>
2021-04-30 12:15:57 -0400

    added basic message handling files
    Signed-off-by: Aliwoto <aminnimaj@gmail.com>

--- wotoPacks/wotoActions/messageType.go
+++ wotoPacks/wotoActions/messageType.go
@@ -1,6 +1,13 @@
+// Rudeus Telegram Bot Project
+// Copyright (C) 2021 wotoTeam, ALiwoto
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE', which is part of the source code.
+
 package wotoActions
 
 import (
+	"github.com/ALiwoto/rudeus01/wotoPacks/wotoSecurity"
+	"github.com/ALiwoto/rudeus01/wotoPacks/wotoValues"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
@@ -25,6 +32,40 @@ func getMessageType(_update *tgbotapi.Update) MessageType {
 }
 
 func _checkTextMessage(_update *tgbotapi.Update) bool {
+	_msg := _update.Message
+	if _msg.MessageID == wotoValues.BaseIndex {
+		return false
+	}
+	if _msg.From == nil {
+		return false
+	}
+	if _msg.Chat == nil {
+		return false
+	}
+	if _msg.Date == wotoValues.BaseIndex {
+		return false
+	}
+	if wotoSecurity.IsEmpty(&_msg.Text) {
+		return false
+	}
+	if _msg.Animation != nil {
+		return false
+	}
+	if _msg.Document != nil {
+		return false
+	}
+	if !wotoSecurity.IsEmpty(&_msg.Caption) {
+		return false
+	}
+	if _msg.Photo != nil {
+		return false
+	}
+	if _msg.Video != nil {
+		return false
+	}
+	if _msg.Game != nil {
+		return false
+	}
 
 	return true
 }
